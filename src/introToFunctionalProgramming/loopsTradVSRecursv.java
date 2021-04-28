80905cc83d9353598508a6393464b84e1589c0f5
David A. <davidaster007@gmail.com>
2021-04-28 10:47:33 +0200

    added loops task, used normal loop and recursive method

---
+++ src/introToFunctionalProgramming/loopsTradVSRecursv.java
@@ -0,0 +1,40 @@
+package introToFunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class loopsTradVSRecursv {
+	public static int arraySum(int[] array) {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+
+		return sum;
+	}
+
+	public static int arraySum2(final int[] array, final int start) {
+		if (start >= array.length) {
+			return 0;
+		}
+		return array[start] + arraySum2(array, start + 1);
+	}
+
+	public static int[] fillArray(int sizeOfArray) {
+		int[] array = new int[sizeOfArray];
+		Random rnd = new Random();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rnd.nextInt(100);
+		}
+
+		return array;
+	}
+
+	public static void main(String[] args) {
+		int[] generatedArray = fillArray(25);
+		System.out.println(Arrays.toString(generatedArray));
+		System.out.println(arraySum(generatedArray));
+		System.out.println(arraySum2(generatedArray, 0));
+
+	}
+}
